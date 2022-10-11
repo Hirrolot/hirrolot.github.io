8460eb1bac2a5e36ab980a3847eb8fc7127e129c
Hirrolot <Hirrolot@users.noreply.github.com>
2022-10-11 11:59:29 +0000

    deploy: 5672270f54a2d678c6ab505d22cc0d9876d804b7

---
+++ script.js
@@ -0,0 +1,64 @@
+window.addEventListener("load", function (event) {
+    genAuthorEmoji();
+});
+
+function genAuthorEmoji() {
+    const emojis = [
+        "💋",
+        "💄",
+        "💅",
+        "❤️",
+        "👄",
+        "👅",
+        "👠",
+        "🌹",
+        "🌺",
+        "🌷",
+        "💐",
+        "🥀",
+        "🍄",
+        "🌹",
+        "🍎",
+        "🍒",
+        "🍓",
+        "🍇",
+        "🍉",
+        "🍌",
+        "🍫",
+        "🍭",
+        "🍬",
+        "🍧",
+        "🍝",
+        "🎈",
+        "💕",
+        "💖",
+        "💘",
+        "💞",
+        "💓",
+        "💝",
+        "💗",
+        "🎀",
+        "🎁",
+        "🌈",
+        "🦄",
+    ];
+
+    const randomEmoji = document.createElement("span");
+    randomEmoji.textContent = emojis[Math.floor(Math.random() * emojis.length)];
+    randomEmoji.innerHTML += "&nbsp;";
+
+    const blogLink = document.createElement("a");
+    blogLink.href = "..";
+    blogLink.textContent = "Hirrolot's Blog";
+
+    const fancyAuthor = document.createElement("p");
+    fancyAuthor.className = "author";
+    fancyAuthor.appendChild(randomEmoji);
+    fancyAuthor.appendChild(blogLink);
+
+    const author = document.querySelector(".author");
+
+    if (author) {
+        author.replaceWith(fancyAuthor);
+    }
+}
